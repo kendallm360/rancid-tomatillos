2bccdcbbe38b0c17fe49ca3bc532d7b51b97fd7d
Kendall McGree <mcgreekendall@gmail.com>
2022-07-07 20:33:29 -0500

    add navbar to the site and update styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import movieData from "./MovieData";
 import AllMovies from "./components/AllMovies";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   constructor() {
@@ -14,7 +15,9 @@ class App extends Component {
     // console.log(this.state.movieList);
     return (
       <div className="App">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <NavBar />
+        </header>
         <body>
           <AllMovies movies={this.state.movieList} />
         </body>
---
+++ src/components/NavBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return <h1>Rancid Tomatillos</h1>;
+};
+
+export default NavBar;
