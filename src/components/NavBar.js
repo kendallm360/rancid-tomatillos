d8bd546af837fb0b350903524cfa8c0ca3f2729e
Kendall McGree <mcgreekendall@gmail.com>
2022-07-18 17:33:45 -0500

    move images to correct folder

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
-import rancid from "./rancid_toma.png";
+import rancid from "../rancid_toma.png";
 
 const NavBar = ({ displayHome }) => {
   return (
